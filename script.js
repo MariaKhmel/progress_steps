a2406ad11bf5dd4589ecb1bf82e5a6d8dd32e6da
Maria Khmel <mariakhmel8@gmail.com>
2025-05-27 21:18:59 +0300

    js

--- script.js
+++ script.js
@@ -7,5 +7,27 @@ let currentActive = 1;
 
 next.addEventListener("click", () => {
   currentActive++;
-  console.log(currentActive);
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
 })
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+
+  update();
+})
+
+function update() {
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  })
+}
